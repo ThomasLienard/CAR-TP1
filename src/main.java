4398c04e6957bfa6d66fff8d93bbab8e2947204d
Murddy <thomas.lienard2@gmail.com>
2024-12-11 11:57:03 +0100

    check username

--- src/main.java
+++ src/main.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
+
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
@@ -13,8 +14,6 @@ public class main {
         ServerSocket serv = new ServerSocket(port);
         Socket s2 = serv.accept();
         System.out.println("Server Ready");
-
-        //PrintStream out = new PrintStream(s2.getOutputStream());
 
         InputStream input = s2.getInputStream();
         OutputStream output = s2.getOutputStream();
@@ -26,11 +25,22 @@ public class main {
 
         System.out.println(str);
 
+        ArrayList<String> logins = new ArrayList<String>();
+        logins.add("miage");
+        logins.add("thomas");
+
+        if (logins.contains(str)) {
+            output.write("331 login OK \r\n".getBytes());
+        }
+        else {
+            output.write("501 Bad Username \r\n".getBytes());
+        }
+
         output.write("331 login OK \r\n".getBytes());
 
         str = scanner.nextLine();
         System.out.println(str);
-       
+
         output.write("230 User logged in \r\n".getBytes());
 
         str = scanner.nextLine();
@@ -45,6 +55,12 @@ public class main {
 
         str = scanner.nextLine();
         System.out.println(str);
+
+        if (str.equals("QUIT")) {
+            scanner.close();
+            s2.close();
+            serv.close();
+        }
 
         scanner.close();
         s2.close();
